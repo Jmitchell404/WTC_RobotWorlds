70fc5b6c8548d6a9bf9e8adbcf86b4b3822d1d7c
niasping021 <niasping021@student.wethinkcode.co.za>
2022-04-29 14:13:33 +0200

    main.02

--- RobotWorlds/src/main/java/configuration/ConfigurationExeptions.java
+++
@@ -1,8 +0,0 @@
-package configuration;
-
-class ConfigurationException extends RuntimeException{
-
-    public ConfigurationException(String errorMessage) {
-        super (errorMessage);
-    }
-}
---
+++ src/main/java/configuration/Configuration.java
@@ -0,0 +1,27 @@
+package configuration;
+
+
+public class Configuration {
+    private int port;
+    private String host;
+
+    //    Port Numbers
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    // IP address
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+
+}
